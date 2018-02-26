9eef2d35790086060e2efaf70929bb25b58dbc0b
zevfang <fangwei0505@gmail.com>
2018-02-26 20:04:06 +0800

    add md5 and uuid

---
+++ system/utils.go
@@ -0,0 +1,21 @@
+package system
+
+import (
+	"github.com/snluu/uuid"
+	"crypto/md5"
+	"encoding/hex"
+
+)
+
+// 计算字符串的md5值
+func Md5(source string) string {
+	md5h := md5.New()
+	md5h.Write([]byte(source))
+	return hex.EncodeToString(md5h.Sum(nil))
+}
+
+func UUID() string {
+	return uuid.Rand().Hex()
+}
+
+
