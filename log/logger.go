5ea6f95255a86bed44004dfd34114d14511517a5
zevfang <fangwei0505@gmail.com>
2018-02-28 19:27:10 +0800

    update log file path

--- log/logger.go
+++ log/logger.go
@@ -3,16 +3,20 @@ package log
 import (
 	"os"
 	"github.com/apsdehal/go-logger"
+	"fmt"
+	"kuaidi100_go/system"
 )
 
 var Mylog *logger.Logger
 
 func NewLogger() {
+
 	var (
 		err     error
 		logFile *os.File
 	)
-	logFile, err = os.OpenFile("logs.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFilePath := fmt.Sprintf("%s%s", system.GetCurrentDirectory(), "/logs.txt")
+	logFile, err = os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic("open log file error.")
 	}
