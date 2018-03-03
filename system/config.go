6b2956fb5c3a32192d75ef3121db337ceb8be171
zevfang <fangwei0505@gmail.com>
2018-03-03 14:55:19 +0800

    add salt

--- system/config.go
+++ system/config.go
@@ -5,20 +5,24 @@ import (
 )
 
 type Configuration struct {
-	Addr           string `ini:"addr"`
-	Key            string `ini:"app_key"`          //订阅推送接口Key（由快递100颁发）
-	SubscribeUrl   string `ini:"subscribe_url"`    //订阅地址
-	CallbackUrl    string `ini:"callback_url"`     //回调地址
-	RedisMaxIdle   int    `ini:"redis_max_idle"`   //最大的空闲连接数
-	RedisMaxActive int    `ini:"redis_max_active"` //最大的激活连接数(同时最多有N个连接)
-	RedisHost      string `ini:"redis_host"`
-	RedisDb        string `ini:"redis_db"`
-	RedisPassWord  string `ini:"redis_pass_word"`
-	MsSqlServer    string `ini:"mssql_server"`
-	MsSqlDataBase  string `ini:"mssql_db"`
-	MsSqlPort      int    `ini:"mssql_port"`
-	MsSqlUid       string `ini:"mssql_uid"`
-	MsSqlPwd       string `ini:"mssql_pwd"`
+	Addr               string `ini:"addr"`
+	Key                string `ini:"app_key"`       //订阅推送接口Key（由快递100颁发）
+	SubscribeUrl       string `ini:"subscribe_url"` //订阅地址
+	CallbackUrl        string `ini:"callback_url"`  //回调地址
+	Salt               string `ini:"salt"`
+	RedisMaxIdle       int    `ini:"redis_max_idle"`   //最大的空闲连接数
+	RedisMaxActive     int    `ini:"redis_max_active"` //最大的激活连接数(同时最多有N个连接)
+	RedisHost          string `ini:"redis_host"`
+	RedisDb            string `ini:"redis_db"`
+	RedisPassWord      string `ini:"redis_pass_word"`
+	MsSqlServer        string `ini:"mssql_server"`
+	MsSqlDataBase      string `ini:"mssql_db"`
+	MsSqlPort          int    `ini:"mssql_port"`
+	MsSqlUid           string `ini:"mssql_uid"`
+	MsSqlPwd           string `ini:"mssql_pwd"`
+	ServiceName        string `ini:"service_name"`         //显示名称
+	ServiceDisplayName string `ini:"service_display_name"` //服务名称
+	ServiceDescription string `ini:"service_description"`  //备注信息
 }
 
 var configuration Configuration
