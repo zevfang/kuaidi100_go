191e16b6e97c779baf780662055fd4773e621dc3
zevfang <fangwei0505@gmail.com>
2018-03-07 15:42:40 +0800

    add Env config

--- system/config.go
+++ system/config.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Configuration struct {
+	Env                string `ini:"env"`
 	Addr               string `ini:"addr"`
 	Key                string `ini:"app_key"`       //订阅推送接口Key（由快递100颁发）
 	SubscribeUrl       string `ini:"subscribe_url"` //订阅地址
@@ -17,14 +18,16 @@ type Configuration struct {
 	RedisPassWord      string `ini:"redis_pass_word"`
 	MsSqlServer        string `ini:"mssql_server"`
 	MsSqlDataBase      string `ini:"mssql_db"`
-	MsSqlPort          int `ini:"mssql_port"`
+	MsSqlPort          int    `ini:"mssql_port"`
 	MsSqlUid           string `ini:"mssql_uid"`
 	MsSqlPwd           string `ini:"mssql_pwd"`
 	ServiceName        string `ini:"service_name"`         //显示名称
 	ServiceDisplayName string `ini:"service_display_name"` //服务名称
 	ServiceDescription string `ini:"service_description"`  //备注信息
-	PollState          int    `json:"poll_state"`          //是否开启订阅
-	PollMinutes        int    `json:"poll_minutes"`        //订阅间隔（秒）
+	PollState          int    `ini:"poll_state"`           //是否开启订阅
+	PollMinutes        int    `ini:"poll_minutes"`         //订阅间隔（秒）
+	OncePollCount      int    `ini:"once_poll_count"`      //每次订阅的数量
+	StartDate          string `ini:"start_date"`           //开始查询时间
 }
 
 var configuration Configuration
