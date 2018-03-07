6d4b9fe72604c76912d11e4fd6fd18c789e13430
zevfang <fangwei0505@gmail.com>
2018-03-07 11:09:46 +0800

    update model

--- model/model.go
+++ model/model.go
@@ -17,27 +17,7 @@ type TradeOrder struct {
 	LogisticsOrder string `db:"logistics_order"`
 }
 
-/*
- 	schema= json
-    param={
-        "company":"ems",
-        "number":"em263999513jp",
-        "from":"广东省深圳市南山区",
-        "to":"北京市朝阳区",
-        "key":"XXX ",
-        "parameters":{
-            "callbackurl":"您的回调接口的地址，如http://www.您的域名.com/kuaidi?callbackid=...",
-            "salt":"XXXXXXXXXX",
-            "resultv2":"1",
-            "autoCom":"1",
-            "interCom"："1",
-            "departureCountry":"CN",
-            "departureCom":"ems",
-            "destinationCountry":"JP",
-            "destinationCom":"japanposten"
-        }
-    }
-*/
+
 // 订阅参数结构
 type PollPostData struct {
 	Schema string `json:"schema"`
@@ -47,48 +27,14 @@ type PollPostData struct {
 		Key     string `json:"key"`
 		Parameters struct {
 			CallbackUrl string `json:"callbackurl"`
-			Salt        string `json:"salt"` //回调使用（签名字符串，随机即可）
+			Salt        string `json:"salt"` //回调使用（签名字符串，随机即可---配置内base64加密码）
 			AutoCom     string `json:"autoCom"`
+			Resultv2    string `json:"resultv2"` //值为1 填写之后推送返回（status、areaCode、areaName）
 		} `json:"parameters"`
 	} `json:"param"`
 }
 
-/*
-	{
-		"status": "polling",
-		"billstatus": "got",
-		"message": "",
-		"autoCheck": "1",
-		"comOld": "yuantong",
-		"comNew": "ems",
-		"lastResult": {
-			"message": "ok",
-			"state": "0",
-			"status": "200",
-			"condition": "F00",
-			"ischeck": "0",
-			"com": "yuantong",
-			"nu": "V030344422",
-			"data": [{
-				"context": "上海分拨中心/装件入车扫描 ",
-				"time": "2012-08-28 16:33:19",
-				"ftime": "2012-08-28 16:33:19",
-				"status": "在途",
-				"areaCode": "310000000000",
-				"areaName": "上海市"
-			}, {
-				"context": "上海分拨中心/下车扫描 ",
-				"time": "2012-08-27 23:22:42",
-				"ftime": "2012-08-27 23:22:42",
-				"status": "在途",
-				"areaCode": "310000000000",
-				"areaName": "上海市"
-
-			}]
-		}
-	}
-*/
-//推送数据结构
+//推送接受数据结构
 type CallPostData struct {
 	Status     string `json:"status"`
 	BillStatus string `json:"billstatus"`
@@ -117,6 +63,37 @@ type CallPostLastResultData struct {
 	AreaName string `json:"areaName"`
 }
 
+// 推送写入库结构
+
+type KdOrder struct {
+	Id           int64  `json:"id" db:"id"`                         //自增编号
+	KdStatus     string `json:"kd_status" db:"kd_status"`           //监控状态:polling:监控中，shutdown:结束，abort:中止，updateall：重新推送
+	KdStatusName string `json:"kd_status_name" db:"kd_status_name"` //转换后的名称
+	KdMessage    string `json:"kd_message" db:"kd_message"`         //监控状态相关消息，如:3天查询无记录，60天无变化  =>abort:中止
+	State        string `json:"state" db:"state"`                   //当前签收状态，包括0在途中、1已揽收、2疑难、3已签收、4退签、5同城派送中、6退回等状态
+	StateName    string `json:"state_name" db:"state_name"`         //当前快递状态名称
+	Com          string `json:"com" db:"com"`                       //（kuaidi100）快递公司编码
+	Shelp        string `json:"shelp" db:"shelp"`                   //（本地）快递公司编码 （需要转换后入库）
+	Nu           string `json:"nu" db:"nu"`                         //快递单号
+	Data         string `json:"data" db:"data"`                     //全量快递信息 json字符串
+	ZtName       string `json:"zt_name" db:"zt_name"`               //0	在途	快件处于运输过程中
+	ZtTime       string `json:"zt_time" db:"zt_time"`
+	LjName       string `json:"lj_name" db:"lj_name"` 				//1	揽件	快件已由快递公司揽收
+	LjTime       string `json:"lj_time" db:"lj_time"`
+	YnName       string `json:"yn_name" db:"yn_name"` 				//2	疑难	快递100无法解析的状态，或者是需要人工介入的状态，比方说收件人电话错误。
+	YnTime       string `json:"yn_time" db:"yn_time"`
+	QsName       string `json:"qs_name" db:"qs_name"` 				//3	签收	正常签收
+	QsTime       string `json:"qs_time" db:"qs_time"`
+	TqName       string `json:"tq_name" db:"tq_name"` 				//4	退签	货物退回发货人并签收
+	TqTime       string `json:"tq_time" db:"tq_time"`
+	PjName       string `json:"pj_name" db:"pj_name"` 				//5	派件	货物正在进行派件
+	PjTime       string `json:"pj_time" db:"pj_time"`
+	ThName       string `json:"th_name" db:"th_name"` 				//6	退回	货物正处于返回发货人的途中
+	ThTime       string `json:"th_time" db:"th_time"`
+	Created      string `json:"created" db:"created"`
+	Updated      string `json:"updated" db:"updated"`
+}
+
 /*
 	{
         "result":true,
@@ -131,7 +108,10 @@ type ResultData struct {
 	Message    string `json:"message" db:"message"`
 }
 
+// 对账存储结构
 type KdSubscribeLog struct {
 	LogisticsOrder string `db:"logistics_order"`
-	ResultData ResultData
+	Result     bool   `db:"result"`
+	ReturnCode string `db:"returnCode"`
+	Message    string `db:"message"`
 }
